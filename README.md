b4a8effef05815926fb7299d272ee37ec7ac7f80
S-Abolfazl-S <s.abolfazl.s.se@gmail.com>
2022-03-20 01:03:20 +0330

    update README,edited index.js, style.css, index.html
    added question's to main content
    
    added timer
    
    get result of quiz
    
    login form and validate users

---
+++ README.md
@@ -0,0 +1,25 @@
+# QUIZ website
+
+<P>
+شرح: 
+<br>
+  در ابتدای ورود به سایت ابتدا از کاربر مشخصاتی گرفته می شود تا هر کاربری وارد امتحان نشود. بعد ورود به امتحان کاربر تا زمان تعیین شده می تواند جواب سوالات را وارد نماید و بعد اتمام زمان نتیجه امتحان داده می شود.
+</P>
+
+### images
+
+![img1](./images/img1.png)
+
+![img2](./images/img2.png)
+
+![img3](./images/img3.png)
+
+### فرم اعتبار سنجی کاربران
+
+### users
+
+- hossein hossein@gmail.com hossein999
+- reza reza@ymail.com reza888
+- sara sara@gmail.com sara777
+
+![img4](./images/img4.png)
--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+// hide page content in coming to website
+document.querySelectorAll("body>:not(form,script)").forEach(e=>{
+    e.style.display='none';
+});
+
+// ~~~~~~~~~~~~~~~~~~ user's list ~~~~~~~~~~~~~~~~~~~~~ // 
+const users = {
+	"hossein":{
+		"email":"hossein@gmail.com",
+		"password":"hossein999",
+		"user_code":101
+	},
+	"reza":{
+		"email":"reza@ymail.com",
+		"password":"reza888",
+		"user_code":102
+	},
+	"sara":{
+		"email":"sara@gmail.com",
+		"password":"sara777",
+		"user_code":103
+	},
+} 
+// ~~~~~~~~~~~~~~~~~~ /user's list ~~~~~~~~~~~~~~~~~~~~~ // 
+
 // ~~~~~~~~~~~~~~~~~~ question's ~~~~~~~~~~~~~~~~~~~~~ //
 const questions = {
 	q1: [
@@ -6,8 +31,8 @@ const questions = {
 		"قسمت های فلزی کامپیوتر", //option1
 		"برد های الکترونیک و درایوها", //option2
 		"وسایل فیزیکی کامپیوتر", //option3
-		"هر سه مورد", //option4
-	], 
+		"هر سه مورد",
+	], //option4
 	q2: [
 		"خصوصیات بارز نسل سوم کامپیوترها چه بود ؟",
 		3,
@@ -81,4 +106,131 @@ const questions = {
 		"موارد ب و ج",
 	],
 };
-// ~~~~~~~~~~~~~~~~~~ /question's ~~~~~~~~~~~~~~~~~~~~~ // 
+// ~~~~~~~~~~~~~~~~~~ /question's ~~~~~~~~~~~~~~~~~~~~~ //
+
+// ~~~~~~~~~~~~~~~~~~ add question's ~~~~~~~~~~~~~~~~~~~~~ // 
+for (let counter = 1; counter <= 10; counter++) {
+	// create template question
+	const nodeMain = document.querySelector("main #quiz-form>div");
+	const newNode = document.createElement("div");
+	newNode.innerHTML = nodeMain.innerHTML;
+	newNode.querySelector("h4").innerHTML = `سوال ${counter}`;
+	newNode.querySelector("p").innerHTML = questions[`q${counter}`][0];
+	//add Option's
+	const opt = newNode.querySelectorAll("div label");
+	opt[0].innerHTML = `الف) ${
+		questions[`q${counter}`][2]
+	} <input type="radio" name="q${counter}" value="1"">`;
+	opt[1].innerHTML = `ب) ${
+		questions[`q${counter}`][3]
+	} <input type="radio" name="q${counter}" value="2"">`;
+	opt[2].innerHTML = `ج) ${
+		questions[`q${counter}`][4]
+	} <input type="radio" name="q${counter}" value="3"">`;
+	opt[3].innerHTML = `د) ${
+		questions[`q${counter}`][5]
+	} <input type="radio" name="q${counter}" value="4"">`;
+	document.querySelector("main #quiz-form").appendChild(newNode);
+}
+// ~~~~~~~~~~~~~~~~~~ /add question's ~~~~~~~~~~~~~~~~~~~~~ // 
+
+
+// ~~~~~~~~~~~~~~~~~~ timer ~~~~~~~~~~~~~~~~~~~~~ // 
+let timer = document.querySelector("header>div.info>.timer");
+let tMinute = 9;
+let tSecond = 60;
+let tStart = () => {
+	tSecond--; 
+	timer.innerHTML = `${tMinute} : ${tSecond}`;
+	if (tSecond <= 0) {
+		tSecond = 60;
+		tMinute--;
+		if (tMinute < 0) {
+			clearInterval(tStop);
+			btnResult.click();
+			window.scrollBy(0, document.body.scrollHeight);
+		} 
+	}
+};
+let tStop = setInterval(tStart, 1000);
+clearInterval(tStop); // stop timer
+// ~~~~~~~~~~~~~~~~~~ /timer ~~~~~~~~~~~~~~~~~~~~~ // 
+
+// ################################## RESULT ################################### //
+// ~~~~~~~~~~~~~~~~~~ get result quiz ~~~~~~~~~~~~~~~~~~~~~ // 
+let rs = 0; //result
+function getResult() { 
+	// first div is template then count variable start from 2
+	for (let count = 2; count <= 11; count++) {
+		// get answer from the question 
+		let ans = document.querySelector(
+			`#quiz-form > div:nth-child(${count}) > div > label > input[type=radio]:checked`
+		);
+		// check input isn't null and validate answer's
+		if (ans !== null) {
+			if (parseInt(ans.value) === questions[`q${count - 1}`][1]) {
+				rs += 2;
+			}
+		}
+	}
+	return rs;
+}
+// ~~~~~~~~~~~~~~~~~~ /get result quiz ~~~~~~~~~~~~~~~~~~~~~ // 
+
+// ~~~~~~~~~~~~~~~~~~ result btn ~~~~~~~~~~~~~~~~~~~~~ // 
+let btnResult = document.querySelector("main div.res button");
+let Result = document.querySelectorAll("main div.res h4");
+let quizForm = document.getElementById("quiz-form");
+
+btnResult.onclick = () => {
+	clearInterval(tStop); // stop time
+	//set value
+	Result[0].innerHTML = `نمره = ${getResult()}`;
+	
+	// fix time result
+	let min = ((9-tMinute)<10)?("0"+(9-tMinute)):(9-tMinute);
+	if(tMinute<=0)
+		min = "00"
+	let sec = ((60-tSecond)<10)?("0"+(60-tSecond)):(60-tSecond); 
+	if(tSecond<=0)
+		sec = "00" 
+
+	Result[1].innerHTML = `${min}:${sec} زمان پاسخ گویی`;
+	//show value
+	Result[0].style.display = "block";
+	Result[1].style.display = "block";
+	// disable form
+	quizForm.style.pointerEvents = "none";
+	btnResult.style.display = "none";
+	// result = 0
+	rs = 0;
+};
+// ~~~~~~~~~~~~~~~~~~ /result btn ~~~~~~~~~~~~~~~~~~~~~ // 
+// ################################## /RESULT ################################### // 
+
+// ~~~~~~~~~~~~~~~~~~ form ~~~~~~~~~~~~~~~~~~~~~ // 
+// register button, click event
+document.querySelector("form").children[3].addEventListener("click", (e) => {
+	// get information
+    const form = document.querySelector("form"); 
+    const username = form.children[0].querySelector('input').value;
+    const email = form.children[1].querySelector('input').value;
+    const password = form.children[2].querySelector('input').value;
+    // validate information
+	if(users[username] !== undefined && users[username]["password"] === password && users[username]["email"] === email){ 
+		// add information : username , gender, 
+		document.querySelector("header>div.info>p").innerHTML = `نام: ${username} شماره: ${users[username].user_code}`;
+		//hide login form
+		document.querySelector("form").style.display = "none";
+		tStop = setInterval(tStart, 1000); // start timer
+		// show content
+		document.querySelectorAll("body>:not(form,script)").forEach(e=>{
+			e.style.display='block';
+		}); 
+	}else{
+		alert('لطفاً مقدار صحیح وارد نمایید');
+	}
+});
+// ~~~~~~~~~~~~~~~~~~ /form ~~~~~~~~~~~~~~~~~~~~~ // 
+
+document.querySelector("form").children[0].querySelector('input').focus()
